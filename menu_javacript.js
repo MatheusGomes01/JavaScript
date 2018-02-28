5462f64163d2e14c05b28c9a6504077855b7c250
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-02-28 15:02:27 -0300

    pagina externa

---
+++ menu_javacript.js
@@ -0,0 +1,7 @@
+/***************** Variaveus **********/
+ /* armazenam uma informação, e atravez disso, conseguimos recuperar dados*/
+ /*INT = Valores inteiros numérios*/
+ /*FLOAR = Armazenam valores fracionados (EX: -2,7)*/
+ /*STRING = COnjunto de Caracteres que formam frases ou outras coisas*/
+ /*BOOLEAN = Variaveis que armazenam estados*/
+alert('oiiie')
