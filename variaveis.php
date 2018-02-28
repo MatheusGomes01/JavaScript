7150fd15e9504634d6d5e1363c36b52f72a2ec0e
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-02-28 15:17:30 -0300

    variaveis de array simples

---
+++ variaveis.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Vartiaveis</title>
+	<script type="text/javascript">
+		
+		var lista_frutas = Array(); // ou = []; ou "= Array('Banana', 'maça' ...);
+		lista_frutas[1] = 'Banana';
+		lista_frutas[2] = 'Maçã';
+		lista_frutas[3] = 'Morango';
+		lista_frutas[4] = 'Uva';
+
+		alert(lista_frutas[3]);
+
+	</script>
+</head>
+<body>
+
+</body>
+</html>
