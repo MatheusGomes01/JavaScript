bd08500bee2ab37c1a4d3d03b786bdb04f7d3ebb
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-02-28 15:02:02 -0300

    aula 1, aprendendo os conceitos

---
+++ Aula1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aula1</title>
+
+	<script type="text/javascript">
+		/*//string
+		var texto = 'Aula de JS';
+
+		//float
+		var numeroFracionado = 10.7;
+
+		//int
+		var numero = -7
+
+		//boolean
+		var teste = true; //true -> verdadeiro //false -> falso
+
+		alert(texto);
+		document.write(numeroFracionado);*/
+
+		var texto = "olá ";
+		var texto2 = prompt('Digite o seu nome');
+
+		document.write(texto + texto2);
+		document.write('<br /><br />');
+	</script>
+</head>
+<body>
+</body>
+</html>
