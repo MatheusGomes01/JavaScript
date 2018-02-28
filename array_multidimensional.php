01c602e0760207f8430031f5ca77bd5626150ed3
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-02-28 15:40:44 -0300

    arrays multidimensionas, array de array

---
+++ array_multidimensional.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Array Multidimensionais</title>
+	<script type="text/javascript">
+		var lista_coisas = Array();
+
+		lista_coisas['frutas'] = Array();
+		lista_coisas['pessoas'] = Array();
+
+
+		lista_coisas['frutas'][1] = 'Banana';
+		lista_coisas['frutas'][2] = 'Morango';
+		lista_coisas['frutas'][3] = 'Abacaxi';
+
+		lista_coisas['pessoas'][1] = 'João'
+		lista_coisas['pessoas'][2] = 'José'
+		lista_coisas['pessoas'][3] = 'Maria'
+
+		console.log(lista_coisas['pessoas'][2]);
+	</script>
+</head>
+<body>
+
+</body>
+</html>
