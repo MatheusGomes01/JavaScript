94bb79cd27d8c3132525962384fe5362b0cb5072
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-01 12:44:25 -0300

    adicionando eventos com as teclas

--- eventos_teclado.php
+++ eventos_teclado.php
@@ -8,7 +8,6 @@
 	  onkeydown --> acionado no momento em que uma tecla é pressionada (soltando ou não o botão)
 	  onkeypress --> acionado no momento em que uma tecla é pressionada (slt/ou/n), porém não captura todas as teclas, como por exemplo alt, crtl, esc, shift, etc.
 	  onkeyup --> acionada no momento em que a tecla é liberada
-	  pode ser usado mais de um elemento
 	*/	
 
 	function msg(){
