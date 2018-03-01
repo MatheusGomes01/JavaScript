2136d812a65f79e5a146e0917c1f3d3a64feb1fa
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-01 11:45:29 -0300

    entendo o laço de repetição do For

---
+++ estrutura_de_repetição_For.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>While</title>
+	<script type="text/javascript">
+		
+
+		for(var x = 1; x < 11; x++){
+			document.write(x + '<br/>')
+
+		}
+	</script>
+</head>
+<body>
+
+</body>
+</html>
