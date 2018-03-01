b207b1a0cdd436efe63b780e25c8323abd43ab4e
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-01 10:53:56 -0300

    switch sabendo usar os comandos

---
+++ switch.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Switch</title>
+	<script type="text/javascript">
+
+	var opcao = 1;
+	
+	switch(opcao){
+		case 1:
+		document.write('Opcao 1 foi selecionada')
+		//se opcao = 1;
+
+		break;
+
+		case 2 :
+		document.write('Opcao 2 foi selecionada')
+		//se opcao = 2;
+
+		break;
+
+		default:
+		document.write('Opcao invalida')
+		//se opcao não encontrada
+	}
+	</script>
+</head>
+<body>
+
+</body>
+</html>
