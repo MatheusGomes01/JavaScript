f2642a69725044cfdb75542544385c381131a9ef
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-01 09:12:31 -0300

    Variaveis if e else com media e nota de aluno

---
+++ if_else.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IF_ELSE</title>
+	<meta charset="utf-8">
+	<script type="text/javascript">
+		
+		var nota = prompt('Digite a nota do aluno');
+		var media = 7; 
+
+		if(nota >= media){
+			document.write('Aprovado');
+		}else {
+			document.write('Reprovado')
+		}
+
+	</script>
+</head>
+<body>
+
+</body>
+</html>
