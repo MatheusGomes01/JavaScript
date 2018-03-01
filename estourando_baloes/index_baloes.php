dcfeaaf2597b820085e797c561f046e12912cabc
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-01 15:55:48 -0300

    imagens estorando boles

---
+++ estourando_baloes/index_baloes.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Estourando balões</title>
+	
+		<script type="text/javascript">
+			
+		</script>
+	</head>
+	<body>
+	
+		<div>
+			<p><b>Nivel1:</b>
+			<select>
+				<option value="1">Fácil</option>
+				<option value="2">Normal</option>
+				<option value="3">Dificil</option>
+			</select>
+			</p>
+
+			<br />
+			<img src="imagens/iniciar.png" onclick="">
+		</div>
+	</body>
+</html>
