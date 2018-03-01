853397e3337a36e6bdf458f02f882abc7edd645f
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-01 13:04:13 -0300

    eventos em formularios

---
+++ eventos_formularios.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Eventos_Formularios</title>
+	<meta charset="utf-8">
+	<script type="text/javascript">
+	/*
+		onfocus - acionado quando o elemento recebe o foco do cursor
+		onblur - acionado quando o elemento perde o foco do cursor
+		onchange - acionado quando o estado do elemento é  modificado
+	 */
+
+	</script> 
+</head>
+
+	<select onchange="alert('a opção foi alterada')">
+		<option value="1">First</option>
+		<option value="2">Second</option>
+	</select>
+<body>
+	
+</body>
+</html>
