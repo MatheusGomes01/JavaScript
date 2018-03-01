32c5ff17df08b9db198d8876961bd9a2b452fb6b
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-01 12:49:21 -0300

    eventos feitos na janela

---
+++ eventos_janela.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Eventos_Janela</title>
+	<meta charset="utf-8">
+	<script type="text/javascript">
+	/*
+	  onload --> acionado quando a pagina finaliza o carregamento
+	  onresize --> acionado quando a pagina ou frame é redirecionado
+	*/	
+
+	</script> 
+</head>
+<body onresize="console.log('The text no sentid')">
+	
+</body>
+</html>
