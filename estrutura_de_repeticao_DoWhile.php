e3e56042d1c39e472057d682a1d594e2d49509c4
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-01 11:40:32 -0300

    estrutura_de_repeticao_DoWhile entendendo o fluxo

---
+++ estrutura_de_repeticao_DoWhile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>While</title>
+	<script type="text/javascript">
+		
+		var x = 1;
+
+		do{
+			document.write(x + '<br />');
+			x = x + 2; //ou //x++;
+
+		}while( x <= 10 );
+	</script>
+</head>
+<body>
+
+</body>
+</html>
