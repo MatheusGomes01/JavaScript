0b900f2e66cceea5d06fd055eccf0453822632fe
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-01 11:30:27 -0300

    while, entendendo os loop's

---
+++ estrutura_de_repeticao_WHILE.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>While</title>
+	<script type="text/javascript">
+		
+		var x = 1;
+		while( x <= 10){
+
+			x++;
+			if( x == 5){
+				continue;
+			}
+
+ 			document.write(x + '<br/>');
+
+ 			/*if (x == 2){
+ 				break;
+ 			}*/
+
+		}
+	</script>
+</head>
+<body>
+
+</body>
+</html>
