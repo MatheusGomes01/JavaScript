ea72432625f0f4795b7be190dda1924dca9737e6
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-01 16:27:54 -0300

    primeiros passos do index de estourar balões

--- index_baloes.php
+++ index_baloes.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
+<html lang="pt-br">
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title>Estourando balões</title>
 	
-		<script type="text/javascript">
+		<script language='Javascript'>
 			
+			function iniciaJogo(){
+				var nivel_jogo = document.getElementById('nivel_jogo').value;
+
+				window.location.href = 'jogo.php?'+nivel_jogo;
+			}
 		</script>
 	</head>
 	<body>
 	
 		<div>
 			<p><b>Nivel1:</b>
-			<select>
+			<select id="nivel_jogo">
 				<option value="1">Fácil</option>
 				<option value="2">Normal</option>
 				<option value="3">Dificil</option>
@@ -20,7 +26,7 @@
 			</p>
 
 			<br />
-			<img src="imagens/iniciar.png" onclick="iniciajogo()" />
+			<img src="estourando_baloes/imagens/iniciar.png" onclick="iniciaJogo()" />
 		</div>
 	</body>
 </html>
